94eaf9ef7bd5206257f3c0f5c16a032bb38f13d4
ndeahl1999 <deahln@rpi.edu>
2019-09-12 16:57:08 -0400

    color scheme and fetch requests

--- client/index.js
+++ client/index.js
@@ -1,3 +1,14 @@
 new Vue({
     el: '#app'
 })
+
+
+function getHealthData(data){
+    fetch('http://localhost:5000?/data?firstname='+data.firstname + "&lastname=" + data.lastname+ "&country="+ data.country + "&ssn=" + data.ssn)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(hData){
+            console.log*hData();
+        });
+}
