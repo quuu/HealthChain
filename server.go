7256c5ca01928e7a7d42fc1aa45d8f5bb7673e4c
ndeahl1999 <deahln@rpi.edu>
2019-09-17 14:25:55 -0400

    go server hosting

---
+++ server.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+
+    http.Handle("/", http.FileServer(http.Dir("./client")))
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+}
