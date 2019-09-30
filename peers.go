e6763b170e5d3da8e8b09ae6af6d20fba15d3e99
Andrew Qu <qua@rpi.edu>
2019-09-30 13:29:03 -0400

    cleaned up codebase

--- peers.go
+++ peers.go
@@ -68,42 +68,4 @@ func main() {
 	time.Sleep(1 * time.Second)
 
 	select {}
-
-	/*
-		listener, err := net.Listen("tcp", ":0")
-		if err != nil {
-			log.WithError(err).Error("unable to create listener")
-			return
-		}
-		port := listener.Addr().(*net.TCPAddr).Port
-		log.Printf("Listening at %s", listener.Addr())
-
-		r := chi.NewRouter()
-		//start the server in the background
-		go func() {
-			err := http.Serve(listener, r)
-			log.WithError(err).Error("unable to listen and serve")
-		}()
-
-		// register service
-		server, err := zeroconf.Register("HealthChain", "_healthchain._tcp", "local.", port, []string{"txtv=0", "lo=1", "la=2"}, nil)
-		if err != nil {
-			panic(err)
-		}
-
-		defer server.Shutdown()
-
-		// Clean exit.
-		sig := make(chan os.Signal, 1)
-		signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-		select {
-		case <-sig:
-			// Exit by user
-		case <-time.After(time.Second * 120):
-			// Exit by timeout
-		}
-
-		log.Println("Shutting down.")
-
-	*/
 }
