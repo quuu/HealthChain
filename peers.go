cf2a1be106a015348dd51b2a531fca0d1d3d00fe
Andrew Qu <qua@rpi.edu>
2019-10-08 22:25:30 -0400

    registers self http service, infinite loop looking for people

--- peers.go
+++ peers.go
@@ -2,39 +2,56 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
+	"net"
+	"net/http"
+
+	//	"strconv"
 	"time"
 	//	"time"
 
 	// "os"
 	//	"net"
-	//	"net/http"
 	//	"os"
 	//	"os/signal"
 	//	"syscall"
 
-	//	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"
 	"github.com/grandcat/zeroconf"
-	"github.com/satori/go.uuid"
+	uuid "github.com/satori/go.uuid"
 	log "github.com/sirupsen/logrus"
 )
 
-var (
-	name     = flag.String("name", "GoZeroconfGo", "The name for the service.")
-	service  = flag.String("service", "_workstation._tcp", "Set the service type of the new service.")
-	domain   = flag.String("domain", "local.", "Set the network domain. Default should be fine.")
-	port     = flag.Int("port", 42424, "Set the port the service is listening to.")
-	waitTime = flag.Int("wait", 10, "Duration in [s] to publish service for.")
-)
+func recordHandler(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func discovery() {
+
+	r := chi.NewRouter()
+	r.Get("/records", recordHandler)
+
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.WithError(err).Error("unable to create listener")
+		return
+	}
+
+	// save the port for registering zeroconf
+	port := listener.Addr().(*net.TCPAddr).Port
+
+	// serve the public record
+	log.Debugf("PeerManager listening at %s", listener.Addr())
+	go func() {
+		err := http.Serve(listener, r)
+		log.WithError(err).Error("PeerManager unable to listen and serve")
+	}()
 
 	// create a unique identifier for this node
 	u := uuid.NewV4()
 
 	// register it with the unique name
-	service, err := zeroconf.Register(u.String(), "_healthchain._tcp", "local.", 8080, nil, nil)
+	service, err := zeroconf.Register(u.String(), "_healthchain._tcp", "local.", port, nil, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -51,21 +68,37 @@ func discovery() {
 	go func(results <-chan *zeroconf.ServiceEntry) {
 		for entry := range results {
 			fmt.Println(entry)
+			fmt.Println(entry.Port)
 		}
 		fmt.Println("out of entries")
 	}(entries)
 
 	// get the background process to browse from
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(*waitTime))
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(10))
 	defer cancel()
 	err = resolver.Browse(ctx, "_healthchain._tcp", "local.", entries)
 	if err != nil {
 		log.Fatalln("Failed to browse:", err.Error())
 	}
-
 	<-ctx.Done()
-	// Wait some additional time to see debug messages on go routine shutdown.
-	time.Sleep(1 * time.Second)
 
-	select {}
+	// HANDLE GLOBAL ENTRIES WITH CLOUD HERE
+	// TODO
+	ticker := time.Tick(1 * time.Second)
+	for {
+		select {
+		case entry := <-entries:
+			handleEntry(entry)
+		case <-ticker:
+			fetchMessages()
+		}
+	}
+}
+
+func handleEntry(entry *zeroconf.ServiceEntry) {
+
+}
+
+func fetchMessages() {
+
 }
