d109234ac2ca8cf7060d3c93244ca8dbdf359242
Andrew Qu <qua@rpi.edu>
2019-10-09 11:30:53 -0400

    made service registration be ran in the background, browsing now on the foreground

--- peers.go
+++ peers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net"
 	"net/http"
 
@@ -51,36 +50,58 @@ func discovery() {
 	u := uuid.NewV4()
 
 	// register it with the unique name
-	service, err := zeroconf.Register(u.String(), "_healthchain._tcp", "local.", port, nil, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer service.Shutdown()
+	go func() {
+		for {
+			server, err := zeroconf.Register(u.String(), "_healthchain._tcp", "local.", port, nil, nil)
+			if err != nil {
+				log.Fatal(err)
+				continue
+			}
+			<-time.After(time.Second * 5)
+			server.Shutdown()
+		}
+	}()
+
+	log.Printf("started listening at %d", port)
 
 	// now browse for other services
+
+	// will store the new peers discovered
+	entries := make(chan *zeroconf.ServiceEntry)
 	resolver, nil := zeroconf.NewResolver(nil)
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
 
-	// make a channel to save the results
-	entries := make(chan *zeroconf.ServiceEntry)
-	go func(results <-chan *zeroconf.ServiceEntry) {
-		for entry := range results {
-			fmt.Println(entry)
-			fmt.Println(entry.Port)
-		}
-		fmt.Println("out of entries")
-	}(entries)
-
-	// get the background process to browse from
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(10))
-	defer cancel()
+	ctx := context.Background()
 	err = resolver.Browse(ctx, "_healthchain._tcp", "local.", entries)
 	if err != nil {
-		log.Fatalln("Failed to browse:", err.Error())
+		log.WithError(err).Error("Unable to browse")
+		return
 	}
-	<-ctx.Done()
+
+	// <-ctx.Done()
+
+	// make a channel to save the results
+	/*
+		go func(results <-chan *zeroconf.ServiceEntry) {
+			for entry := range results {
+				fmt.Println(entry)
+				fmt.Println(entry.Port)
+			}
+			fmt.Println("out of entries")
+		}(entries)
+
+		// get the background process to browse from
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(10))
+		defer cancel()
+		err = resolver.Browse(ctx, "_healthchain._tcp", "local.", entries)
+		if err != nil {
+			log.Fatalln("Failed to browse:", err.Error())
+		}
+		<-ctx.Done()
+	*/
 
 	// HANDLE GLOBAL ENTRIES WITH CLOUD HERE
 	// TODO
@@ -96,6 +117,8 @@ func discovery() {
 }
 
 func handleEntry(entry *zeroconf.ServiceEntry) {
+	log.Println("got an entry")
+	log.Println(entry.Port)
 
 }
 
