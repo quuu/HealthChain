723a2e78477f1dc5e9a43df200fbb5f1949213d1
Noah Deahl <32648923+ndeahl1999@users.noreply.github.com>
2019-11-01 14:17:41 -0400

    New record display and upload (#16)
    * records component
    
    * initial message display
    
    * new upload data
    
    * fixed saving to use multi part form data

---
+++ client/components/appointment.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+
+        <b-collapse class="card" aria-id="contentIdForA11y3" :open="false">
+            <div
+                slot="trigger" 
+                slot-scope="props"
+                class="card-header"
+                role="button"
+                aria-controls="contentIdForA11y3">
+                <p class="card-header-title">
+                  {{formatDate(date)}}
+                </p>
+                <a class="card-header-icon">
+                    <b-icon
+                        :icon="props.open ? 'menu-down' : 'menu-up'">
+                    </b-icon>
+                </a>
+            </div>
+            <div class="card-content">
+                <div class="content">
+                    {{message}}
+                </div>
+            </div>
+        </b-collapse>
+    </section>
+</template>
+
+
+<script>
+import moment from 'moment';
+export default {
+  
+
+  props:['date', 'message'],
+  methods:{
+    formatDate(date){
+      var myDate = new Date(date);
+      return moment(myDate).format('MMMM Do YYYY, h:mm:ss a');
+    }
+  }
+}
+</script>
