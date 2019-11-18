c18e1be588bfbbfa8a433939479287bb22c00706
ndeahl1999 <deahln@rpi.edu>
2019-11-18 13:04:29 -0500

    first multiple fields addition

--- client/pages/uploads.vue
+++ client/pages/uploads.vue
@@ -55,22 +55,43 @@
                           <div class="field column"></div>
                           
                           
+                        </div>
+                        <div class="columns">
                           <div class="field column">
+                            Appointment Information:
+                            
+                            <b-input class="control" type="textarea" placeholder="Summary"></b-input>
+
+                            <div class="control">
+                              Fields:
+                                <b-field v-for="i in numFields" :key="i" >
+                                    <b-select class="column is-2" placeholder="Select Field">
+                                      <option value="height">Height</option>
+                                      <option value="weight">Weight</option>
+                                      <option value="vaccination">Vaccination</option>
+                                      <option value="weight">Weight</option>
+                                      <option value="sickness">Sickness</option>
+                                      <option value="eyesight">Eyesight</option>
+                                      <option value="blood-pressure">Blood Pressure</option>
+                                    </b-select>
+                                    <b-input class ="column is-6" placeholder="Value" ></b-input>
+                                    <div class="column is-1" ><b-button class="is-danger" icon-left="delete" @click="numFields--"></b-button></div>
+                               </b-field>
+                            </div>
+                          </div>
+                          
+                        </div>
+                        <b-button rounded @click="numFields++">
+                            Add Field
+                          </b-button>
+
+                      <div class="field column is-pulled-right">
                               <button class="button is-danger" @click.prevent=" () => {
                                 postHealthData()
                                 }" >
                                   Upload Records 
                               </button>
                           </div>
-                        </div>
-                        <div class="columns">
-                          <div class="field column">
-                            Appointment Information:
-                            <div class="control">
-                                <textarea class="textarea is-danger" placeholder="e.g. Routine Checkup" v-model="message"></textarea>
-                              </div>
-                          </div>
-                        </div>
                       <b-loading :is-full-page="false" :active.sync="isLoading" :can-cancel="true">
                       </b-loading>
                     </form>
@@ -93,8 +114,9 @@ export default {
       lastname: null,
       country: null,
       code: null,
-      message: null,
-      isLoading: false
+      message: {},
+      isLoading: false,
+      numFields:1
     
     };
   },
@@ -114,6 +136,7 @@ export default {
     },
 
     async postHealthData(){
+      console.log(message);
       this.openLoading();
       let self =this;
       await axios({
@@ -136,7 +159,7 @@ export default {
         if(response.data == "Saved!"){
           Notification.open({
                     duration: 5000,
-                    message: `Records uploaded successfully!`,
+                    message: `Record for `+ self.firstname + ` uploaded successfully!`,
                     position: 'is-bottom-right',
                     type: 'is-success',
                     hasIcon: true
