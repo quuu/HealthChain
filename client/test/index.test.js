c6f55913941a3233a8ac773e56581cb5d0837f37
Noah Deahl <32648923+ndeahl1999@users.noreply.github.com>
2019-11-21 15:39:37 -0500

    Front end testing: Created Ava Tests (#25)
    * Initial Front End Testing
    
    * Front end route testing
    
    * package lock
    
    * removed package lock

---
+++ client/test/index.test.js
@@ -0,0 +1,56 @@
+import { resolve } from 'path'
+import { Nuxt, Builder } from 'nuxt'
+import { JSDOM } from 'jsdom'
+import test from 'ava'
+
+// We keep the nuxt and server instance
+// So we can close them at the end of the test
+let nuxt = null
+
+// Init Nuxt.js and create a server listening on localhost:4000
+test.before(async () => {
+  const config = {
+    dev: false,
+    rootDir: resolve(__dirname, '..')
+  }
+  nuxt = new Nuxt(config)
+  await new Builder(nuxt).build()
+  await nuxt.server.listen(4000, 'localhost')
+}, 30000)
+
+// Example of testing only generated html
+test('Route / exists and render HTML', async (t) => {
+  const context = {}
+  const { html } = await nuxt.server.renderRoute('/', context)
+  t.true(html.includes('<h1 class="title">Welcome to HealthChain!</h1>'))
+})
+
+test('Route /records exists and render HTML', async (t) => {
+  const context = {}
+  const { html } = await nuxt.server.renderRoute('/records', context)
+  t.true(html.includes('<p class="title is-3 has-text-centered">Health Records Request</p>'))
+})
+
+
+test('Route /uploads exits and render HTML', async (t) => {
+  const context = {}
+  const { html } = await nuxt.server.renderRoute('/uploads', context)
+  t.true(html.includes('<p class="title is-3 has-text-centered">Health Record Submission</p>'))
+})
+
+// Example of testing via dom checking
+// test('Route / exits and render HTML with CSS applied', async (t) => {
+//   const context = {}
+//   const { html } = await nuxt.server.renderRoute('/', context)
+//   const { window } = new JSDOM(html).window
+//   const element = window.document.querySelector('.red')
+//   t.not(element, null)
+//   t.is(element.textContent, 'Hello world!')
+//   t.is(element.className, 'red')
+//   t.is(window.getComputedStyle(element).color, 'red')
+// })
+
+// Close server and ask nuxt to stop listening to file changes
+test.after('Closing server and nuxt.js', (t) => {
+  nuxt.close()
+})
